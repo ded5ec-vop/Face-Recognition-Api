08c580f26e757412ebc94556c768c15003ed92c8
ded5ec-vop <zvcreeper01@gmail.com>
2021-11-26 15:59:29 +0400

    Changed knex.js db connection details

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '0103',
-    database : 'face_rec'
+  	connectionString: process.env.DATABASE_URL
+  	ssl: {
+  		rejectUnauthorized: false
+  	}
+    //host : '127.0.0.1',
+    //user : 'postgres',
+    //password : '0103',
+    //database : 'face_rec'
   }
 });
 
