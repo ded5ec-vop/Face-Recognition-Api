c695c0413f1bd79be1e0ec245a3e8e04f2b48f3e
ded5ec-vop <zvcreeper01@gmail.com>
2021-11-26 14:22:31 +0400

    Adding Port

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 //PORT number declaration
 //------------------------------------------------------------->
-app.listen(3000, () => {
-	console.log('App is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`App is running on port ${process.env.PORT}`);
 })
